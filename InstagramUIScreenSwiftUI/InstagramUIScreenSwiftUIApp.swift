d1a8db21635947882cc59da28d7e63f16ed5857b
chuljin Hwang <chljin.hwang@gmail.com>
2022-02-24 16:39:24 +0900

    Initial Commit

---
+++ InstagramUIScreenSwiftUI/InstagramUIScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUIScreenSwiftUIApp.swift
+//  InstagramUIScreenSwiftUI
+//
+//  Created by Chuljin Hwang on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUIScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
